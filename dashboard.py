0c053968a1ba3c6148b095494f1ab4824201d127
Iara <iaramescuacastro@gmail.com>
2022-11-14 02:47:25 -0300

    Update Notebook and Dashboard File

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+import seaborn as sns
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv('exams.csv')
